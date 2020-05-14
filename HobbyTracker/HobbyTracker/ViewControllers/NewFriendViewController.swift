a5d7398e07b18e3e0c3e0f4693e04ff65adcd597
violet-love <violetlove9419@icloud.com>
2020-05-14 09:43:02 -0700

    added cocoaTouchFiles, named things accordingly, created outlets for the text fields on the NewFriendViewController

---
+++ HobbyTracker/HobbyTracker/ViewControllers/NewFriendViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewFriendViewController.swift
+//  HobbyTracker
+//
+//  Created by Violet Lavender Love on 5/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class NewFriendViewController: UIViewController {
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var homeTownTextField: UITextField!
+    @IBOutlet var hobby1TextField: UITextField!
+    @IBOutlet var hobby2TextField: UITextField!
+    @IBOutlet var hobby3TextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+     
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HobbyTracker/HobbyTracker/Views/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  HobbyTracker
+//
+//  Created by Violet Lavender Love on 5/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
