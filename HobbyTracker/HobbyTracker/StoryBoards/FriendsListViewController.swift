2987a374cec9bf8685696773a6ee47dfcd729307
violet-love <violetlove9419@icloud.com>
2020-05-14 11:43:16 -0700

    finished setting up the delegate protocal so that i could add friends to the friends list view

--- HobbyTracker/HobbyTracker/StoryBoards/FriendsListViewController.swift
+++ HobbyTracker/HobbyTracker/StoryBoards/FriendsListViewController.swift
@@ -21,17 +21,21 @@ class FriendsListViewController: UIViewController {
         tableView.dataSource = self
 
     }
-    
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ModalNewFriend" {
+            
+            let newFriendVC = segue.destination as? NewFriendViewController
+            
+            //"Hey newFriendVC I'm the to talk to in order to add a friend to the friends array"
+            newFriendVC?.delegate = self
+        }
+     
     }
-    */
+  
 
 }
 
@@ -44,8 +48,23 @@ extension FriendsListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as? FriendTableViewCell else {
+            fatalError("Cell identifer is not of expected type FriendTableView. If you're a user, I'm sorry your app just crashed. Feel free to send me an angry letter.")
+        }
+        let friend = friends[indexPath.row]
+        
+        cell.nameLabel.text = friend.name
+        cell.homeTownLabel.text = friend.homeTown
+        cell.hobbiesLabel.text = "\(friend.hobbies.count) hobby(ies)"
         
         return cell
+    }
+}
+
+//I will follow the rules to add a new friend
+extension FriendsListViewController: AddFriendDelegate {
+    func friendWasCreated(friend: Friend) {
+        friends.append(friend)
+        tableView.reloadData()
     }
 }
